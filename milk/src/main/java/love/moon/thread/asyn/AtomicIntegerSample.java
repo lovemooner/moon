bf9841df3e78b7b06ccca67bb7305b6e1bc58dfd
lovemooner <lovemooner@yeah.net>
2017-06-15 18:25:28 +0800

    thread

--- milk/src/main/java/love/moon/thread/asyn/AtomicIntegerSample.java
+++ milk/src/main/java/love/moon/thread/asyn/AtomicIntegerSample.java
@@ -1,8 +1,33 @@
 package love.moon.thread.asyn;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Author: lovemooner
  * Date: 2017/6/14 16:54
  */
 public class AtomicIntegerSample {
+    public AtomicInteger inc = new AtomicInteger();
+
+    public void increase() {
+        inc.getAndIncrement();
+    }
+
+    public static void main(String[] args) {
+        final AtomicIntegerSample test = new AtomicIntegerSample();
+        for (int i = 0; i < 10; i++) {
+            new Thread() {
+                public void run() {
+                    for (int j = 0; j < 1000; j++)
+                        test.increase();
+                }
+
+                ;
+            }.start();
+        }
+
+        while (Thread.activeCount() > 1)  //保证前面的线程都执行完
+            Thread.yield();
+        System.out.println(test.inc);
+    }
 }
---
+++ milk/src/main/java/love/moon/thread/asyn/VolatileSample.java
@@ -0,0 +1,8 @@
+package love.moon.thread.asyn;
+
+/**
+ * Author: lovemooner
+ * Date: 2017/6/15 16:34
+ */
+public class VolatileSample {
+}

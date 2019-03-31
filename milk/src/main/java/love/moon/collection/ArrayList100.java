9483da52edb42364d0f86a2ad25639d557764a24
lovemooner <lovemooner@yeah.net>
2019-04-01 00:10:23 +0800

    lambda

--- milk/src/main/java/love/moon/collection/ArrayList100.java
+++ milk/src/main/java/love/moon/collection/ArrayList100.java
@@ -1,6 +1,7 @@
 package love.moon.collection;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -11,5 +12,9 @@ public class ArrayList100 {
     public static void main(String[] args) {
         List list=new ArrayList();
         list.add("a");
+
+            List<String> list2 = Collections.synchronizedList(new ArrayList<String>());
+            list2.add("a");
+            list2.add("a");
     }
 }

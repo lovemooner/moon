9d48bcb1518fc2212b604d0aafb8565bd722a202
lovemooner <lovemooner@yeah.com>
2019-04-08 18:08:35 +0800

    Thread future mode

---
+++ milk/src/main/java/love/moon/thread/future/CompletableFuture101.java
@@ -0,0 +1,24 @@
+package love.moon.thread.future;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFuture101 {
+
+    public static void main(String[] args) {
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() ->{
+            return Thread.currentThread().getName();
+        });
+//        doSomethingelse();//做你想做的耗时操作
+
+        try {
+            System.out.println(completableFuture.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}

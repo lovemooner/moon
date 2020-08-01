2708d2f8b95abc38ee01ed8add223351105cf2ca
dongnan <dongnan@kedacom.com>
2020-08-01 20:47:23 +0800

    algorithm

---
+++ algorithm/src/main/java/love/moon/algorithm/sort/BubbleSort.java
@@ -0,0 +1,30 @@
+package love.moon.algorithm.sort;
+
+/**
+ * 冒泡排序
+ *
+ * @author dongnan
+ * @date 2020/7/28 15:35
+ */
+public class BubbleSort {
+
+    public int[] sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            // 设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序，排序已经完成。
+            boolean flag = true;
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                    flag = false;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+        return arr;
+    }
+
+}

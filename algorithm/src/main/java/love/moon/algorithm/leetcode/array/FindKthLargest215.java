f72b6a5536b701954f87b6c4b24e42e1550e8574
lovemooner <lovemooner@yeah.net>
2021-04-27 23:01:38 +0800

    动态规划

--- algorithm/src/main/java/love/moon/algorithm/leetcode/array/FindKthLargest215.java
+++ algorithm/src/main/java/love/moon/algorithm/leetcode/array/FindKthLargest215.java
@@ -7,7 +7,7 @@ public class FindKthLargest215 {
     public static void main(String[] args) {
 //        int [] nums={3,2,3,1,2,4,5,5,6};
         int [] nums={1,2,2,3,3,4,5,5,6};
-        System.out.println(findKthLargest(nums,4));
+        System.out.println(findKthLargest(nums,2));
     }
 
     public static int findKthLargest(int[] nums, int k) {
